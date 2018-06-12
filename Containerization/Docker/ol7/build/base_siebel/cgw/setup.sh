f8744d6ffc32847cbc7dc94bfb20a8c4e7bccc24
Girish Pradhan <33644652+gsrspradhan@users.noreply.github.com>
2018-06-12 00:04:14 -0700

    Simplifying the shell script
    Combining multiple chown and chgrp statements into a single statement

--- Containerization/Docker/ol7/build/base_siebel/cgw/setup.sh
+++ Containerization/Docker/ol7/build/base_siebel/cgw/setup.sh
@@ -1,6 +1,4 @@
-chown -R siebel /mnt/Siebel_Enterprise_Server
-chgrp -R siebel /mnt/Siebel_Enterprise_Server
-chown -R siebel siebel
-chgrp -R siebel siebel
+chown -R siebel:siebel /mnt/Siebel_Enterprise_Server
+chown -R siebel:siebel siebel
 
 su siebel -c "/mnt/Siebel_Enterprise_Server/Disk1/install/runInstaller.sh -silent -responseFile /config/cgw.rsp -invPtrLoc /config/oraInst.loc -waitforcompletion -showProgress -oneclick"
